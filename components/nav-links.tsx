13b78b3284b8375cb9811333233e3386f3299f0e
ottati_nicolas <ottatinicolas91@gmail.com>
2025-01-17 11:49:13 +0400

    final touches for now made.

--- components/nav-links.tsx
+++ components/nav-links.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function NavLinks() {
   return (
-    <div className="flex gap-4">
-      <Link className="hover:text-gray-500 duration-200 fade-in" href="/">Home</Link>
-      <Link className="hover:text-gray-500 duration-200 fade-in" href="/about">About</Link>
-      <Link className="hover:text-gray-500 duration-200 fade-in" href="/contact">Contact</Link>
-      <Link className="hover:text-gray-500 duration-200 fade-in" href="/experience">Experience</Link>
-      <Link className="hover:text-gray-500 duration-200 fade-in" href="/projects">Projects</Link>
+    <div className="flex gap-10 transition-all duration-200 fade-in-out">
+      <Link className="hover:text-gray-500 duration-200 fade-in-out" href="/">Home</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in-out" href="/about">About</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in-out" href="/contact">Contact</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in-out" href="/experience">Experience</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in-out" href="/projects">Projects</Link>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,9 @@ import NavSocials from "./nav-socials";
 
 export default async function Navbar() {
   return (
-    <div className="flex justify-between items-center p-4 bg-gray-200">
+    <div className="flex justify-between items-center p-4 bg-white shadow-md">
       <div className="flex flex-col items-start">
-        <div className="text-2xl font-bold">Nicolas Ottati</div>
+        <div className="text-2xl text-gray-700 font-bold">Nicolas Ottati</div>
         <div className="text-sm text-gray-500">Junior Developer | Event & Project Manager</div>
       </div>
       <NavLinks />
