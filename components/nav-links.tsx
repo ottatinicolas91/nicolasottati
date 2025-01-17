919685d591265c5f9c1ef781516ba96461f07fc2
ottati_nicolas <ottatinicolas91@gmail.com>
2025-01-17 11:26:09 +0400

    added animation for all nav links.

--- components/nav-links.tsx
+++ components/nav-links.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function NavLinks() {
   return (
     <div className="flex gap-4">
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
-      <Link href="/experience">Experience</Link>
-      <Link href="/projects">Projects</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in" href="/">Home</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in" href="/about">About</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in" href="/contact">Contact</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in" href="/experience">Experience</Link>
+      <Link className="hover:text-gray-500 duration-200 fade-in" href="/projects">Projects</Link>
     </div>
   );
 }
--- components/nav-socials.tsx
+++ components/nav-socials.tsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 export default function NavSocials() {
   return (
     <div className="flex gap-4">
-      <Link target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ottati_dj/">
+      <Link target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ottati_dj/" className="text-gray-700 hover:text-gray-500 duration-200 fade-in">
         <SiInstagram strokeWidth={1} />
       </Link>
-      <Link target="_blank" rel="noopener noreferrer" href="https://github.com/ottatinicolas91">
+      <Link target="_blank" rel="noopener noreferrer" href="https://github.com/ottatinicolas91" className="text-gray-700 hover:text-gray-500 duration-200 fade-in">
         <SiGithub strokeWidth={1} />
       </Link>
-      <Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nicolas-ottati/">
+      <Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nicolas-ottati/" className="text-gray-700 hover:text-gray-500 duration-200 fade-in">
         <Linkedin strokeWidth={1.5} />
       </Link>
     </div>
