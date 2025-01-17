624f233d351aec12299fc033131a753d32b8e66d
ottati_nicolas <ottatinicolas91@gmail.com>
2025-01-17 11:21:14 +0400

    added simple icons library for deprecated brand icons. Setted up links for my socials.

--- components/nav-socials.tsx
+++ components/nav-socials.tsx
@@ -1,3 +1,21 @@
+import { Linkedin } from "lucide-react";
+import { SiGithub, SiInstagram } from "@icons-pack/react-simple-icons";
+
+import Link from "next/link";
+
+
 export default function NavSocials() {
-  return <div>NavSocials</div>;
+  return (
+    <div className="flex gap-4">
+      <Link target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ottati_dj/">
+        <SiInstagram strokeWidth={1} />
+      </Link>
+      <Link target="_blank" rel="noopener noreferrer" href="https://github.com/ottatinicolas91">
+        <SiGithub strokeWidth={1} />
+      </Link>
+      <Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nicolas-ottati/">
+        <Linkedin strokeWidth={1.5} />
+      </Link>
+    </div>
+  );
 }
