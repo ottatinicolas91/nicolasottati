ec3f474ef119e207d6ce2d2ea45f68a9a2261110
ottati_nicolas <ottatinicolas91@gmail.com>
2025-01-20 12:38:05 +0400

    Styling for footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,12 @@ import NavSocials from "./nav-socials";
 
 export default function Footer() {
   return (
-    <footer className="bg-white py-6 mt-auto">
+    <footer className="bg-white py-8 mt-auto relative">
+      <div className="bg-gradient-to-r from-transparent via-gray-200 to-transparent h-1 absolute top-0 left-0 right-0"></div>
       <div className="flex justify-between items-center p-4">
-        <p className="text-sm text-gray-500">&copy; 2023 Nicolas Ottati. All rights reserved.</p>
+        <p className="text-sm text-gray-500">
+          &copy; 2023 Nicolas Ottati. All rights reserved.
+        </p>
         <p className="text-sm text-gray-500">Developed by Nicolas Ottati</p>
         <NavSocials />
       </div>
