4d76732ea1bda165cb3102f0b04ceee3de27afcf
ottati_nicolas <ottatinicolas91@gmail.com>
2025-01-20 16:52:04 +0400

    new folder structure implementing project routes created.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import ContainerBlock from "@/components/container-block";
+
+export default function ProjectsPage() {
+  return (
+    <ContainerBlock>
+      <div>
+        <h1 className="text-3xl font-bold">Projects</h1>
+      </div>
+    </ContainerBlock>
+  );
+}
