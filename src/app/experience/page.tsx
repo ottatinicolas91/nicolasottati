e3d4bf54ba93eb17b5f04788568c7014753068f5
ottati_nicolas <ottatinicolas91@gmail.com>
2025-01-22 13:52:46 +0400

    Created Cards and implemented some dummy data fetching from some random url. Need to create a propper db in the near future.

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,11 +1,26 @@
 import ContainerBlock from "@/components/container-block";
+import CardExperience from "@/components/card-experience";
+import CardExperienceDivider from "@/components/card-experience-divider";
+import { getExperience } from "@/lib/fetchers";
 
-export default function ExperiencePage() {
+export default async function ExperiencePage() {
+  const experience = await getExperience();
   return (
     <ContainerBlock>
-      <div>
-        <h1 className="text-3xl font-bold">Experience</h1>
-      </div>
+      <section className="bg-white dark:bg-black mx-auto">
+        <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-black">
+          <h1 className="text-5xl md:text-9xl font-bold py-20 text-center md:text-left dark:text-white">
+            Experience
+          </h1>
+        </div>
+        <div className="px-4 bg-[#F1F1F1] -mt-10 min-h-dvh dark:bg-neutral-900"> {/*remember to change to the correct height */}
+          <div className="flex flex-col items-center min-w-xl max-w-6xl mx-auto pt-32 pb-20">
+            {experience.map((experience) => (
+              <CardExperience experience={experience} />
+            ))}
+          </div>
+        </div>
+      </section>
     </ContainerBlock>
   );
 }
